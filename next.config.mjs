4042052abdcb62cc3e07b47529a3969dd6ea9361
fratellinho <yves.matro@gmail.com>
2026-01-28 12:31:46 +0100

    fix: add supabase domain to next.config.mjs images

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: '*.cjdropshipping.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'yefzblyovihrvoqcqjtg.supabase.co',
+            },
         ],
     },
 };
